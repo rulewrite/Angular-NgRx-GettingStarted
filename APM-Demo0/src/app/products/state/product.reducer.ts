799335ed332ff73562c5c33adbc44a4ad38f40ee
rulewrite <rulewrite@gmail.com>
2020-10-19 02:25:53 +0900

    feat: 상품 리듀서 생성 및 바인딩

---
+++ APM-Demo0/src/app/products/state/product.reducer.ts
@@ -0,0 +1,8 @@
+import { createAction, createReducer, on } from '@ngrx/store';
+
+export const productReducer = createReducer(
+  {
+    showProductCode: true
+  },
+  on(createAction('[Product] Toggle Product Code'), state => ({...state, showProductCode: !state.showProductCode}))
+);
