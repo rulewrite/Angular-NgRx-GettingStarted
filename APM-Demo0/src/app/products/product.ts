5a67421781dbfc16e51c309bacea043b8ee8c377
rulewrite <rulewrite@gmail.com>
2020-11-01 23:26:42 +0900

    refactor: Product 인터페이스를 클래스로 대체

--- APM-Demo0/src/app/products/product.ts
+++ APM-Demo0/src/app/products/product.ts
@@ -1,8 +1,21 @@
 /* Defines the product entity */
+
+/*
 export interface Product {
     id: number | null;
     productName: string;
     productCode: string;
     description: string;
     starRating: number;
+}
+*/
+
+export class Product {
+  constructor(
+    public id: number | null,
+    public productName: string,
+    public productCode: string,
+    public description: string,
+    public starRating: number
+  ) {}
 }
