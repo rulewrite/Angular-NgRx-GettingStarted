75686b5aa7610d18f2bf6318a33bc95aad87d82e
rulewrite <rulewrite@gmail.com>
2020-12-03 01:37:16 +0900

    feat: 상품 생성 이펙트 추가

--- APM-Demo0/src/app/products/state/product.effects.ts
+++ APM-Demo0/src/app/products/state/product.effects.ts
@@ -55,4 +55,18 @@ export class ProductEffects {
       )
     );
   });
+
+  createProduct$ = createEffect(() => {
+    return this.actions$.pipe(
+      ofType(ProductActions.createProduct),
+      mergeMap((action) =>
+        this.productService.createProduct(action.product).pipe(
+          map((product) => ProductActions.createProductSuccess({ product })),
+          catchError((error) =>
+            of(ProductActions.createProductFailure({ error }))
+          )
+        )
+      )
+    );
+  });
 }
